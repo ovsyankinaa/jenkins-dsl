819f686adcdb0293a721eb5e482e681f1e52d7dc
Aliaksandr Ausiankin <aliaksandr_ausiankin@epam.com>
2022-03-21 11:25:05 +0300

    disable filter in active choice

--- job.groovy
+++ job.groovy
@@ -3,7 +3,6 @@ job("day6/MNTLAB-aausiankin-main-build-job"){
     stringParam('BRANCH_NAME', 'jenkins-dsl', 'Branche name')
     activeChoiceParam('choose jobs for execution') {
       description('User can choose jobs for execution')
-        filterable()
         choiceType('CHECKBOX')
         groovyScript {
         script('["MNTLAB-aausiankin-child1-build-job", "MNTLAB-aausiankin-child2-build-job", "MNTLAB-aausiankin-child3-build-job", "MNTLAB-aausiankin-child4-build-job"]')
