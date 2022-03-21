b3853b2ab51930c60cb0d9852e7937bce5aae554
Aliaksandr Ausiankin <aliaksandr_ausiankin@epam.com>
2022-03-21 21:48:03 +0300

    script for child jobs v11

--- job.groovy
+++ job.groovy
@@ -33,23 +33,22 @@ job("day6/MNTLAB-aausiankin-child1-build-job"){
       description('Branch name')
         choiceType('SINGLE_SELECT')
         groovyScript {
-        script('''def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git" 
+        script('''def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git"
 def command = "git ls-remote -h $gitURL"
 
-                def proc = command.execute()
-                proc.waitFor()              
+def proc = command.execute()
+proc.waitFor()
 
-                if ( proc.exitValue() != 0 ) {
-                  println "Error, ${proc.err.text}"
-                  System.exit(-1)
-                }
+if ( proc.exitValue() != 0 ) {
+  println "Error, ${proc.err.text}"
+  System.exit(-1)
+}
 
-                def branches = proc.in.text.readLines().collect { 
-                  it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '') 
-                }     
+def branches = proc.in.text.readLines().collect {
+  it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
+}
 
-                return branches
-              ''')
+return branches''')
         fallbackScript()
       }
     }
