44e307017dc101e092a0afd787ae521b7782bbc6
Aliaksandr Ausiankin <aliaksandr_ausiankin@epam.com>
2022-03-21 11:03:23 +0300

    main job initial

---
+++ job.groovy
@@ -0,0 +1,30 @@
+job(MNTLAB-aausiankin-main-build-job){
+  parameters {
+    stringParam('BRANCH_NAME', 'jenkins-dsl', 'Branche name')
+    activeChoiceParam('choose jobs for execution') {
+      description('User can choose jobs for execution')
+        filterable()
+        choiceType('CHECK_BOXES')
+        groovyScript {
+        script('["MNTLAB-aausiankin-child1-build-job", "MNTLAB-aausiankin-child2-build-job", "MNTLAB-aausiankin-child3-build-job", "MNTLAB-aausiankin-child4-build-job"]')
+        fallbackScript()
+      }
+    }
+  }  
+}
+
+job(MNTLAB-aausiankin-child1-build-job){
+  
+}
+
+job(MNTLAB-aausiankin-child2-build-job){
+  
+}
+  
+job(MNTLAB-aausiankin-child3-build-job){
+  
+}
+
+job(MNTLAB-aausiankin-child4-build-job){
+  
+}
