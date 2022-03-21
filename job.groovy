df6164b35982b20ecb76f6d13bc8569204f7eb3c
Aliaksandr Ausiankin <aliaksandr_ausiankin@epam.com>
2022-03-21 21:19:03 +0300

    script for child jobs

--- job.groovy
+++ job.groovy
@@ -28,6 +28,31 @@ job("day6/MNTLAB-aausiankin-main-build-job"){
 }
 
 job("day6/MNTLAB-aausiankin-child1-build-job"){
+  parameters {
+    activeChoiceParam('BRANCH_NAME') {
+      description('Branch name')
+        choiceType('SINGLE_SELECT')
+        groovyScript {
+        script('def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git" 
+                def command = "git ls-remote -h $gitURL"
+
+                def proc = command.execute()
+                proc.waitFor()              
+
+                if ( proc.exitValue() != 0 ) {
+                  println "Error, ${proc.err.text}"
+                  System.exit(-1)
+                }
+
+                def branches = proc.in.text.readLines().collect { 
+                  it.replaceAll(/[a-z0-9]*\trefs\/heads\//, "") 
+                }     
+
+                return branches')
+        fallbackScript()
+      }
+    }
+  }  
   steps {
     shell('sleep 20')
   }  
