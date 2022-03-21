90188440f71f981a3006aff30edf40227cbe3f08
Aliaksandr Ausiankin <aliaksandr_ausiankin@epam.com>
2022-03-21 22:21:39 +0300

    main job predefined parameters v2

--- job.groovy
+++ job.groovy
@@ -24,7 +24,7 @@ job("day6/MNTLAB-aausiankin-main-build-job"){
           configs {
             predefinedBuildParameters {
               properties('BRANCH_NAME=$BRANCH_NAME')
-              textParamValueOnNewLine(True)
+              textParamValueOnNewLine('True')
             }
           } 
         }
