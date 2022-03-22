b1ff5b58109a2ef9a95858fdf5f712dede586f04
Aliaksandr Ausiankin <aliaksandr_ausiankin@epam.com>
2022-03-22 10:08:06 +0300

    child job changes

--- job.groovy
+++ job.groovy
@@ -67,63 +67,17 @@ job("day6/MNTLAB-aausiankin-child1-build-job"){
 
 job("day6/MNTLAB-aausiankin-child2-build-job"){
   parameters {
-    activeChoiceParam('BRANCH_NAME') {
-      description('Branch name')
-        choiceType('SINGLE_SELECT')
-        groovyScript {
-        script('''def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git"
-def command = "git ls-remote -h $gitURL"
-
-def proc = command.execute()
-proc.waitFor()
-
-if ( proc.exitValue() != 0 ) {
-  println "Error, ${proc.err.text}"
-  System.exit(-1)
-}
-
-def branches = proc.in.text.readLines().collect {
-  it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
-}
-
-return branches
-''')
-        fallbackScript()
-      }
-    }
-  }  
+    stringParam('BRANCH_NAME', 'main', 'Branche name')
+  }
   steps {
     shell('sleep 20')
   }  
 }
-  
+
 job("day6/MNTLAB-aausiankin-child3-build-job"){
   parameters {
-    activeChoiceParam('BRANCH_NAME') {
-      description('Branch name')
-        choiceType('SINGLE_SELECT')
-        groovyScript {
-        script('''def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git"
-def command = "git ls-remote -h $gitURL"
-
-def proc = command.execute()
-proc.waitFor()
-
-if ( proc.exitValue() != 0 ) {
-  println "Error, ${proc.err.text}"
-  System.exit(-1)
-}
-
-def branches = proc.in.text.readLines().collect {
-  it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
-}
-
-return branches
-''')
-        fallbackScript()
-      }
-    }
-  }  
+    stringParam('BRANCH_NAME', 'main', 'Branche name')
+  }
   steps {
     shell('sleep 20')
   }  
@@ -131,31 +85,8 @@ return branches
 
 job("day6/MNTLAB-aausiankin-child4-build-job"){
   parameters {
-    activeChoiceParam('BRANCH_NAME') {
-      description('Branch name')
-        choiceType('SINGLE_SELECT')
-        groovyScript {
-        script('''def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git"
-def command = "git ls-remote -h $gitURL"
-
-def proc = command.execute()
-proc.waitFor()
-
-if ( proc.exitValue() != 0 ) {
-  println "Error, ${proc.err.text}"
-  System.exit(-1)
-}
-
-def branches = proc.in.text.readLines().collect {
-  it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
-}
-
-return branches
-''')
-        fallbackScript()
-      }
-    }
-  }  
+    stringParam('BRANCH_NAME', 'main', 'Branche name')
+  }
   steps {
     shell('sleep 20')
   }  
