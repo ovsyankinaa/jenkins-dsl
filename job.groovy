339c9925de25b474a39307580aec73d2f24a2517
Aliaksandr Ausiankin <aliaksandr_ausiankin@epam.com>
2022-03-22 10:03:36 +0300

    main job second active choice parameter

--- job.groovy
+++ job.groovy
@@ -1,6 +1,29 @@
 job("day6/MNTLAB-aausiankin-main-build-job"){
   parameters {
-    stringParam('BRANCH_NAME', 'jenkins-dsl', 'Branche name')
+    activeChoiceParam('BRANCH_NAME') {
+      description('Branch name')
+        choiceType('SINGLE_SELECT')
+        groovyScript {
+        script('''def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git"
+def command = "git ls-remote -h $gitURL"
+
+def proc = command.execute()
+proc.waitFor()
+
+if ( proc.exitValue() != 0 ) {
+  println "Error, ${proc.err.text}"
+  System.exit(-1)
+}
+
+def branches = proc.in.text.readLines().collect {
+  it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
+}
+
+return branches
+''')
+        fallbackScript()
+      }
+    }
     activeChoiceParam('CHILD_JOBS_NAMES') {
       description('User can choose jobs for execution')
         choiceType('CHECKBOX')
@@ -35,31 +58,7 @@ job("day6/MNTLAB-aausiankin-main-build-job"){
 
 job("day6/MNTLAB-aausiankin-child1-build-job"){
   parameters {
-    activeChoiceParam('BRANCH_NAME') {
-      description('Branch name')
-        choiceType('SINGLE_SELECT')
-        groovyScript {
-        script('''def gitURL = "https://github.com/ovsyankinaa/jenkins-dsl.git"
-def command = "git ls-remote -h $gitURL"
-
-def proc = command.execute()
-proc.waitFor()
-
-if ( proc.exitValue() != 0 ) {
-  println "Error, ${proc.err.text}"
-  System.exit(-1)
-}
-
-def branches = proc.in.text.readLines().collect {
-  it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
-}
-
-return branches
-''')
-        fallbackScript()
-      }
-    }
-  }  
+    stringParam('BRANCH_NAME', 'main', 'Branche name')
   steps {
     shell('sleep 20')
   }  
